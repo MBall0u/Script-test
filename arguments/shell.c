35ecb9690f3f154a66e51f3f629422f03418f134
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-19 18:13:57 -0500

    skjdbfjshdb

--- arguments/shell.c
+++ arguments/shell.c
@@ -9,22 +9,26 @@ int main(void)
 	char *argv[] = {NULL, NULL}, *sep = "\n",*buf = NULL;
 	size_t size = 64;
 	ssize_t e_check;
+	pid_t cmd;
 
 	while (1)
 	{
 		printf("$ ");
-		e_check = getline(&buf, &size, stdin);
-		if (e_check == -1)
+		cmd = fork();
+		if (cmd == 0)
 		{
-			printf("Something went wrong!\n");
-			break;
+			e_check = getline(&buf, &size, stdin);
+			if (e_check == -1)
+			{
+				printf("Something went wrong!\n");
+				break;
+			}
+
+			argv[0] = strtok(buf, sep);
+			printf("%s", argv[0]);
+
+			execve(argv[0], argv, NULL);
 		}
-		
-		argv[0] = strtok(buf, sep);
-		printf("%s", argv[0]);
-
-		execve(argv[0], argv, NULL);
-
 		free(buf);
 		buf = NULL;
 	}
