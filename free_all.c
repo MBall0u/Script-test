817b9fe592c249c21c4d99fcc09d2ed617ea68e2
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-07-29 14:34:38 -0500

    ksdnjsb

---
+++ free_all.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * 
+ * 
+ * 
+*/
+void free_all(char **args, char **path_args, char *buf, char *temp)
+{
+	free(buf); /*frees dynamically allocated memory for str*/
+	buf = NULL;
+	free(args); /*frees dynamically allocated memory for args*/
+	args = NULL;
+	free(path_args);
+	path_args = NULL;
+	free(temp);
+	temp = NULL;
+}
--- michael.c
+++ michael.c
@@ -1,52 +1,15 @@
 #include "main.h"
 int main(void)
 {
-	char *sep = " \t\r\n:;="; /*sep is the separators used for strtok, word is for the token, and str is for a dynamic version of buf*/
-	size_t size = 64; /*size is a general buffer amount, but it is only being used for getline and it is actually being ignored because buf is NULL*/
-	ssize_t check;
-	char **args, **path_args; /*args is used for an array of char pointer to be dynamically allocated*/
-	char *buf = NULL; /*buf is for getline to dynamically alloced inside the function, when buf is NULL the bufsize is ignored*/
-	extern char **environ;
-	char *path, *temp = NULL;
-
 	if (isatty(0))
-		printf("interactive");
-	else
-		printf("non-interactive");
-
-	while (1)
 	{
-		printf("$ "); /*with print at the beginning of each line no matter what*/
-
-		check = getline(&buf, &size, stdin); /*get input from standard input and dynamically allocates the needed memory to buf, ignoring size*/
-		if (check == -1) /*check to see if getline failed*/
-		{
-			buf = NULL;
-			break;
-		}
-
-		args = get_tokens(buf, sep); /*makes an array of args from the string buf from getline*/
-		path = path_get(environ); /*gets the path from the environment*/
-		path_args = get_tokens(path, sep); /*makes an array of args from the path returned from the above function*/
-		temp = check_build(args[0], path_args); /*gets a workable path and puts it in postion 0 of the args array*/
-		if (temp == NULL)
-		{
-			perror("Cannot find command\n");
-			exit(EXIT_FAILURE);
-		}
-		else
-		{
-			args[0] = temp;
-			function_call(args, environ);
-		}
-		free(buf); /*frees dynamically allocated memory for str*/
-		buf = NULL;
-		free(args); /*frees dynamically allocated memory for args*/
-		args = NULL;
-		free(path_args);
-		path_args = NULL;
-		free(temp);
-		temp = NULL;
+		printf("interactive");
+		loop_for_interactive();
+	}
+	else
+	{
+		printf("non-interactive");
+		non_interactive();
 	}
 	return (0);
 }
