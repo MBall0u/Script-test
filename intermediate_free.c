3fc862add7e951ad7a7de5ba920d4aa1d338c0a1
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-01 17:55:26 -0500

    ksdnjsb

--- intermediate_free.c
+++ intermediate_free.c
@@ -4,14 +4,15 @@
  * 
  * 
 */
-void free_all(char ***args, char ***path_args, char **buf, char **temp)
+void intermediate_free(char **buf, char ***args)
 {
+	int i;
 	free(*buf); /*frees dynamically allocated memory for str*/
 	*buf = NULL;
+	for (i = 0; (*args)[i] != NULL; i++)
+	{
+		free((*args)[i]);
+	}
 	free(*args); /*frees dynamically allocated memory for args*/
 	*args = NULL;
-	free(*path_args);
-	*path_args = NULL;
-	free(*temp);
-	*temp = NULL;
 }
