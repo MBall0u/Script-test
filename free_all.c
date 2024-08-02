91f343c3772722703871b5be837b4655a6c3f716
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-08-01 21:12:33 -0500

    ksdnjsb

--- free_all.c
+++ free_all.c
@@ -9,7 +9,7 @@ void free_all(char ***args, char ***path_args, char **buf, char **temp)
 	int i;
 	free(*buf); /*frees dynamically allocated memory for str*/
 	*buf = NULL;
-	for (i = 0; *args[i] != NULL; i++)
+	for (i = 0; (*args)[i] != NULL; i++)
 		free(*args[i]);
 	free(*args); /*frees dynamically allocated memory for args*/
 	*args = NULL;
